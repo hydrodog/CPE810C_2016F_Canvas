4b71b6d5a8aed0d12206e53b56c86f7fa20ea2b4
tfalsone <tfalsone@users.noreply.github.com>
2016-11-02 14:24:08 -0400

    Add files via upload
    Initial curving files

---
+++ curving.cc
@@ -0,0 +1,32 @@
+#include "curving.h"
+
+curving::curving()
+{
+	curving() {
+		
+	}
+	
+	void lin_curve() {
+		
+	}
+	
+	void root_curve() {
+		
+	}
+	
+	double return_curve() {
+		
+	}
+	
+	double curve_mean() {
+		
+	}
+	
+	double curve_median() {
+		
+	}
+	
+	double curve_variance() {
+		
+	}
+}
