4906887d705aa2f64e8c0f4082d85cee37402c4f
Bhavitha Dasaram <bdasaram@stevens.edu>
2016-11-14 17:47:51 -0500

    added few comments and modified mainwindow.cpp

--- Graph_V1/mainwindow.cpp
+++ Graph_V1/mainwindow.cpp
@@ -22,63 +22,73 @@ MainWindow::~MainWindow()
 }
 void MainWindow::setupgraph(QCustomPlot *customPlot){
         int numberoffiles;
-        cout <<"enter number of assignment files\n";
-        cin >> numberoffiles;
+        cout <<"Enter total number of  files\n";
+        cin >> numberoffiles;   // files include assignments,midterm (or) tests
         vector<Student> student;
-        // for(int i=1; i<= numberoffiles; i++){
         ifstream filename("a1.txt");
         for(int k=1;k<=numberoffiles;k++){
-          int assignmenttype;
-          if (filename.is_open()){
-              filename>> assignmenttype;
-              Student::Set_AssignmentType(assignmenttype);
-              int i=1;
-              while( i<=10){
-                  Student s;
-                  int id;
-                  double grade=0;
-                  filename >> id;
-                  s.Setstudent_id(id);
-                  filename >> grade;
-                  if(assignmenttype==1){
-                      if(s.Get_numAssignments() == 0){
-                          s.SetAssignment_Marks(grade);
-                      }
-                      else{
-                          for(unsigned int j=0; j< student.size(); j++){
-                              Student& s2 = student.at(j);
-                              if(s2.Getstudent_id() == id){
-                                  s2.SetAssignment_Marks(grade);
-                              }
-                          }
-                      }
+        int assignmenttype;
+        if (filename.is_open()){
+          filename>> assignmenttype;
+          Student::Set_AssignmentType(assignmenttype);
+          int i=1;
+          while(!filename.eof()){
+              Student s;
+              int id;
+              double grade=0;
+              filename >> id;
+              if(filename.eof())
+                  break;
+              s.Setstudent_id(id);
+              filename >> grade;
+
+              /* check for assignment type and then reading grades inthe same
+                 order as in the txt file  */
+
+              if(assignmenttype==1){
+                  if(s.Get_numAssignments() == 0){
+                      s.SetAssignment_Marks(grade);
                   }
 
-                  else if (assignmenttype == 2){
-                      if(s.Get_numTests() == 0)
-                          s.SetTest_Marks(grade);
-                      else
-                          for(unsigned int j=0; j< student.size(); j++){
-                              Student& s2 = student.at(j);
-                              if(s2.Getstudent_id() == id){
-                                  s2.SetAssignment_Marks(grade);
-                              }
+              /* as data in 2 different files is not in same order
+                  following check is done*/
+
+                  else{
+                      for(unsigned int j=0; j< student.size(); j++){
+                          Student& s2 = student.at(j);
+                          if(s2.Getstudent_id() == id){
+                              s2.SetAssignment_Marks(grade);
+                              break;
                           }
+                      }
                   }
-                  if(k == 1)
-                      student.push_back(s);
-                  i++;
               }
+
+              else if (assignmenttype == 2){
+                  if(s.Get_numTests() == 0)
+                      s.SetTest_Marks(grade);
+                  else
+                      for(unsigned int j=0; j< student.size(); j++){
+                          Student& s2 = student.at(j);
+                          if(s2.Getstudent_id() == id){
+                              s2.SetAssignment_Marks(grade);
+                              break;
+                          }
+                      }
+              }
+              if(k == 1)
+                  student.push_back(s);
+              i++;
           }
-          else
-              cout<<"not open\n";
-          filename.close();
-          if(assignmenttype==1)
-              Student::Set_numAssignments();
-          else if(assignmenttype==2)
-              Student::Set_numTests();
-          std::cout<<"opening a2.txt"<<endl;
-          filename.open("a2.txt");
+        }
+        else
+          cout<<"not open\n";
+        filename.close();
+        if(assignmenttype==1)
+          Student::Set_numAssignments();
+        else if(assignmenttype==2)
+          Student::Set_numTests();
+        filename.open("a2.txt");
         }
 
         //using scatter graph
