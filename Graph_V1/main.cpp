2d9467a806a632ee1995233def38b6cb71e75180
shriraj <snale@stevens.edu>
2016-11-17 08:43:25 -0500

    sort entered test scores in ascending order+ mean
    Flow of program
    *Dynamically allocate an array to hold the number of test scores
    *Calculate the mean test score
    *Arrange the scores in ascending order
    *Free dynamically allocated memory

--- Graph_V1/main.cpp
+++ Graph_V1/main.cpp
@@ -1,11 +1,83 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
-int main(int argc, char *argv[])
+void arraySort(double scores[], int numScores);
+
+
+void arraySort(double scores[], int numScores)
+
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
+ bool exchanges;
+ do
+ {
+  exchanges = false;                                                           // assume no exchanges
+  for(int i = 0; i < numScores - 1; i++){
+   if(scores[i] > scores[i + 1]){
+    double temp = scores[i];
+    scores[i] = scores[i+1];
+    scores[i+1] = temp;
+    exchanges = true;                                                               // after exchange, must look again
+   }
+  }
+ }
+ while(exchanges);
 }
+
+
+int main (void)
+{
+ double *scores,                                                          //To dynamically allocate an array
+ total = 0.0,                                                            // Accumulator
+ mean;                                                                  // To hold average scores
+ int numScores,                                                        //To hold the number of test scores
+ count;                                                               //Counter variable
+
+
+ cout << "How many test scores would you like to enter? ";
+ cin >> numScores;
+
+
+
+ scores = new double[numScores];                                         //Dynamically allocate an array large enough to hold that many test scores
+
+
+ cout << "Enter the test scores below.\n";
+
+ for (count = 0; count < numScores; count++)
+ {
+  cout << "Test Score " << (count + 1) << ": ";
+  cin >> scores[count];
+ }
+
+
+ for (count = 0; count < numScores; count++)
+ {
+  total += scores[count];                         //Calculate the total of the scores
+ }
+
+
+ mean = total / numScores;                                               //Calculate the mean test score
+
+
+ cout << fixed << showpoint << setprecision(2);
+ cout << "Mean score is: " << mean << endl;
+
+
+ arraySort(scores,numScores);
+
+ for (count = 0; count < numScores; count++)
+ {
+  cout<<"score "<<(count+1)<<": "<<scores[count]<<endl;
+ }
+
+
+                                                                                //Free dynamically allocated memory
+ delete [] scores;
+ scores = 0;                                                                     //Make scores point to null.
+ cin.ignore(INT_MAX, '\n');
+ cin.ignore(INT_MAX, '\n');
+
+ return 0;
+}
+
