00319d98a43ddc3cd101c2a12542ff65b38f651b
Chun Ji <278183391@qq.com>
2016-11-29 12:05:20 -0500

    Update Canvas_Update.cpp

--- Canvas_Update/Canvas_Update.cpp
+++ Canvas_Update/Canvas_Update.cpp
@@ -40,18 +40,3 @@ Canvas_Update::writeRequest(grade_info *gradeList,int Post_size)
 	CURLcode ret = curl_easy_perform(hnd);	
 }
 
-Canvas_Update::writeRequest(grade_info *gradeList,int Post_size)
-{
-	// the following is the example about how to update the grade and comment on sumbmission, and it is work!
-	// but there could be anyother API to grade or comment on multiple submissions
-	// we could work on it too
-	CURL *hnd = curl_easy_init();	
-	curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "PUT");
-	curl_easy_setopt(hnd, CURLOPT_URL, "https://sit.instructure.com/api/v1/courses/133/assignments/46024/submissions/19850");
-	struct curl_slist *headers = NULL;
-	headers = curl_slist_append(headers, "authorization: Bearer 1030~4MvxMX4ZFH8LCUAe9pFKhNUwFc6o4PnsFny5vlO4sLdyExxx3B9SR3ULM2nzl211");
-	headers = curl_slist_append(headers, "accept: application/json");	headers = curl_slist_append(headers, "content-type: application/json");
-	curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "{\"comment\":{\"text_comment\" : \"asdfasdgasdfasdfasdf\"},\"submission\":{\"posted_grade\": \"99\"} }");
-	curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
-	CURLcode ret = curl_easy_perform(hnd);	
-}
