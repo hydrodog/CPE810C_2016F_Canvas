7cfdf588c58a17035210231ae55e85da229e0def
abuhse <abuhse32@gmail.com>
2016-12-02 03:00:29 -0500

    Updated the test file to make use of more functions

--- Plagiarism_Detector_Ver01/Test.cpp
+++ Plagiarism_Detector_Ver01/Test.cpp
@@ -7,16 +7,18 @@ using namespace std;
 
 int main (){
 
-  detectPlagiarism c;
-  string s = "Hello";
-  string a = "Hello";
-  vector<string> i;
-  vector<string> b;
-  i = c.getTokens(s);
-  b = c.getTokens(a);
-  cout << c.LCS(i,b) << endl;
+  detectPlagiarism test = detectPlagiarism();
+  string sa = "Hello this is the first sentence";
+  string sb = "Hello this is the second sentence";
+  vector<string> student1 = test.getTokens(sa);
+  vector<string> student2 = test.getTokens(sb);
+  vector<vector<string>> students = vector<vector<string>>();
+  students.push_back(student1);
+  students.push_back(student2);
+  cout << test.LCS(student1,student2) << endl;
+  cout << test.singleLcsTest(students, 1).at(0) << endl;
 
-
+  system("pause");
   return 0;
 
 }
