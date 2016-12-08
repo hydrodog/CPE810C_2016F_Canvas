3666303161a01c07477e1b876a37962263e5b554
Yliuinstevens <yliu166@stevens.edu>
2016-12-08 13:18:25 -0500

    Update Canvas_Update.cpp

--- Canvas_Update/Canvas_Update.cpp
+++ Canvas_Update/Canvas_Update.cpp
@@ -1,42 +1,60 @@
-#include <CanvasUpdate.h>
+#include "Canvas_Update.h"
+#include <fstream>
 
-Canvas_Update::Canvas_Update(char *greads_file_dir,int courseId,int AssignmentsId, string auth,int Post_size))
+
+
+Canvas_Update::Canvas_Update(char * greads_file_dir, int courseId, int AssignmentsId, string auth)
 {
 	greads_file_dir = greads_file_dir;
-	courseId = courseId;	AssignmentsId = AssignmentsId;
-	auth = auth;
-	Post_size = Post_size//how many grades we need to update;
-	update_Api = API + "course/" + courseId + "/assignments/"+AssignmentsId+"/submissions/";
+	courseId = courseId;	
+	AssignmentsId = AssignmentsId;
+	auth = "authorization: Bearer " + auth;
+	update_Api = "https://sit.instructure.com/api/v1/course/" + to_string(courseId) + "/assignments/" + to_string(AssignmentsId) + "/submissions/";
+	getGradeInfo(greads_file_dir);
+	convertData(gradeList);
+	sendRequest();
+
 }
 
-void Canvas_Update::getGradeInfo(*greads_file_dir)
+void Canvas_Update::getGradeInfo(char * greads_file_dir)
 {
-	// this part is to get the grades data from the grade file given by Homework Automation group
-	// reade the file data and build the list of grade (*gradeList)
-	// Here We should know the struct about the file, we need to make a agreement on it
+	//FILE *gradefile;
+	//read the file and get the data into gradelist
+	//here is for test
+	grade_info stu1;
+	stu1.Stu_ID = "19850";
+	stu1.group_comment = false;
+	stu1.posted_grade = "11";
+	stu1.text_comment = "dont worry, it is just a test";
+	gradeList.push_back(stu1);
+
 }
 
-string Canvas_Update::convertData(grade_info oneGrade)
+void Canvas_Update::convertData(vector<grade_info> gradeList)
 {
-	//make the request body like this  
-	//{\"comment\":{\"text_comment\" : \"asdfasdgasdfasdfasdf\"},\"submission\":{\"posted_grade\": \"99\"} }");
-	string commend = "";
-	commend += "{\"comment\":{\"text_comment\" : \"" + oneGrade.text_comment + "\"},\"submission\":{\"" + oneGrade.posted_grade + "\"} }";
-	return string(commend);
-}
-Canvas_Update::writeRequest(grade_info *gradeList,int Post_size)
-{
-	// the following is the example about how to update the grade and comment on sumbmission, and it is work!
-	// but there could be anyother API to grade or comment on multiple submissions
-	// we could work on it too
-	CURL *hnd = curl_easy_init();	
-	curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "PUT");
-	curl_easy_setopt(hnd, CURLOPT_URL, "https://sit.instructure.com/api/v1/courses/133/assignments/46024/submissions/19850");
-	struct curl_slist *headers = NULL;
-	headers = curl_slist_append(headers, "authorization: Bearer 1030~4MvxMX4ZFH8LCUAe9pFKhNUwFc6o4PnsFny5vlO4sLdyExxx3B9SR3ULM2nzl211");
-	headers = curl_slist_append(headers, "accept: application/json");	headers = curl_slist_append(headers, "content-type: application/json");
-	curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "{\"comment\":{\"text_comment\" : \"asdfasdgasdfasdfasdf\"},\"submission\":{\"posted_grade\": \"99\"} }");
-	curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
-	CURLcode ret = curl_easy_perform(hnd);	
+	for (int i = 0; i < gradeList.size(); i++)
+	{
+		reqBody[i] = "{\"comment\":{\"text_comment\" : \"" + gradeList[i].text_comment + "\"},\"submission\":{\"" + gradeList[i].posted_grade + "\"} }";
+		stuIdList[i] = gradeList[i].Stu_ID;
+	}
+
 }
 
+void Canvas_Update::sendRequest()
+{
+	for (int i = 0; i < gradeList.size(); i++)
+	{
+		    string puturl = update_Api + stuIdList[i];
+			CURL *hnd = curl_easy_init();
+			curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "PUT");
+			curl_easy_setopt(hnd, CURLOPT_URL, puturl);
+			struct curl_slist *headers = NULL;
+			headers = curl_slist_append(headers, auth);
+			headers = curl_slist_append(headers, "accept: application/json");
+			headers = curl_slist_append(headers, "content-type: application/json");
+			curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, reqBody[i]);
+			curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+			CURLcode ret = curl_easy_perform(hnd);
+	}
+	
+}
