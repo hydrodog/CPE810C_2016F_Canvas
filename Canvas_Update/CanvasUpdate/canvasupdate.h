9997be18a5d7389c2bf962106268730c2c91ecc6
Jozyzhou <Jozyzhou@users.noreply.github.com>
2016-12-15 15:54:11 -0500

    Update canvasupdate.h

--- Canvas_Update/CanvasUpdate/canvasupdate.h
+++ Canvas_Update/CanvasUpdate/canvasupdate.h
@@ -5,7 +5,32 @@
 #include <QString>
 #include <QFile>
 #include <vector>
+#include <String>
 using namespace std;
+
+struct grade_information //define the single submission grades struct
+{
+    double  Stu_ID;
+    string text_comment; //add a textual comment to the submission.
+    double posted_grade;//the grade of the submission
+
+    /*the fellowing is the extra data we could put into the Canvas
+    bool   group_comment;//whether or not this comment should be sent to the entire group(defaults to false).ignored if this is not a group assignment or if no text_comment is provided.
+    string media_comment_id//add an audio/video comment to the submission. media comments can be added via this api
+    string media_comment_type//type of media comment being added.allow audio, video
+    int      comment_file_ids//Attach files to this comment that were previously uploaded using the Submission Comment API's files action
+    string visibility//Whether this assignment is visible to the owner of the submission
+    bool   excuse;//Sets the “excused” status of an assignment.*/
+};
+
+//String to String
+//std::string str = "Hello world";
+//QString qstr = QString::fromStdString(str);
+
+
+//double valueAsDouble = 1.2;
+//QString valueAsString = QString::number(valueAsDouble);
+
 
 struct grade_info //define the single submission grades struct
 {
@@ -38,6 +63,8 @@ private:
 public:
     Canvas_Update(QString courseId, QString AssignmentsId, QString auth);
     Canvas_Update(vector<grade_info> gradeList, QString courseId, QString AssignmentsId, QString auth);//construction function
+    Canvas_Update(vector<grade_information> gradeList, long courseId,long AssignmentsId);
+    
     //void getGradeInfo(char *greads_file_dir);
     void convertData(vector<grade_info> dataList);	//convert the grades data into string type
     void sendRequest();//send the request to Canvas
