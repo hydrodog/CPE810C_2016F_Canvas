22eaca977c94330f248c9addad189851ec7dfb80
Jozyzhou <Jozyzhou@users.noreply.github.com>
2016-12-15 16:13:57 -0500

    Update canvasupdate.h

--- Canvas_Update/CanvasUpdate/canvasupdate.h
+++ Canvas_Update/CanvasUpdate/canvasupdate.h
@@ -8,15 +8,11 @@
 #include <String>
 using namespace std;
 
-
 struct grade_information //define the single submission grades struct
 {
     double  Stu_ID;
     string text_comment; //add a textual comment to the submission.
     double posted_grade;//the grade of the submission
-
-    grade_information(double stu_id, double sub_grade, string sub_comment) :
-        Stu_ID(stu_id), posted_grade(sub_grade), text_comment(sub_comment) {}
 
     /*the fellowing is the extra data we could put into the Canvas
     bool   group_comment;//whether or not this comment should be sent to the entire group(defaults to false).ignored if this is not a group assignment or if no text_comment is provided.
@@ -26,8 +22,6 @@ struct grade_information //define the single submission grades struct
     string visibility//Whether this assignment is visible to the owner of the submission
     bool   excuse;//Sets the “excused” status of an assignment.*/
 };
-
-
 
 //String to String
 //std::string str = "Hello world";
@@ -43,6 +37,13 @@ struct grade_info //define the single submission grades struct
     QString Stu_ID;
     QString text_comment; //add a textual comment to the submission.
     QString posted_grade;//the grade of the submission
+    grade_info(grade_information grade)
+    {
+        Stu_ID = QString::number(grade.Stu_ID);
+        text_comment = QString::fromStdString(grade.text_comment);
+        posted_grade = QString::number(grade.posted_grade);
+    }
+
 
     /*the fellowing is the extra data we could put into the Canvas
     bool   group_comment;//whether or not this comment should be sent to the entire group(defaults to false).ignored if this is not a group assignment or if no text_comment is provided.
@@ -70,7 +71,7 @@ public:
     Canvas_Update(QString courseId, QString AssignmentsId, QString auth);
     Canvas_Update(vector<grade_info> gradeList, QString courseId, QString AssignmentsId, QString auth);//construction function
     Canvas_Update(vector<grade_information> gradeList, long courseId,long AssignmentsId);
-    
+
     //void getGradeInfo(char *greads_file_dir);
     void convertData(vector<grade_info> dataList);	//convert the grades data into string type
     void sendRequest();//send the request to Canvas
